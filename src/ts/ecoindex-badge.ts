11733efa51b6ce8b0e341d2872cdc3f997090ec5
Vincent Vatelot <vincent.vatelot@ik.me>
2024-02-20 17:33:36 +0100

    fix(timeout): change bff url to test behavior with timeout

--- src/ts/ecoindex-badge.ts
+++ src/ts/ecoindex-badge.ts
@@ -5,7 +5,7 @@ interface ApiResponseInterface {
     };
 }
 
-const baseUrl = 'https://bff.ecoindex.fr';
+const baseUrl = 'https://tasks.ecoindex.fr';
 const badge: HTMLElement | null = document.getElementById('ecoindex-badge');
 const LightTheme = 'light';
 const isLightTheme: boolean =
